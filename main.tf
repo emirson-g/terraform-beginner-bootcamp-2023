bad320f78ba310741fbaf2875e44fa5fb5370672
emirson-g <emirsonm@gmail.com>
2024-04-26 08:37:08 -0400

    #5 Refactor AWS CLI script

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
-#test gitpod
-#test github
-echo "test"
-echo "test2"
-echo "test3"
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.1"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+resource "random_string" "bucket_name" {
+  length           = 16
+  special          = false
+  
+}
+output "random_bucket_name_id" {
+    value = random_string.bucket_name.result
+}
