3e273024f93497986cdfc0b76f72f949800cd60c
emirson-g <127235012+emirson-g@users.noreply.github.com>
2024-04-26 17:38:17 -0400

    #6 Add S3 Bucket (#7)

--- main.tf
+++ main.tf
@@ -4,16 +4,36 @@ terraform {
       source = "hashicorp/random"
       version = "3.6.1"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }    
   }
 }
 
 provider "random" {
   # Configuration options
 }
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
 resource "random_string" "bucket_name" {
-  length           = 16
+  length           = 32
   special          = false
+  upper            = false
   
+}
+
+resource "aws_s3_bucket" "bucket_random_s3" {
+  bucket = random_string.bucket_name.result
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 output "random_bucket_name_id" {
     value = random_string.bucket_name.result
